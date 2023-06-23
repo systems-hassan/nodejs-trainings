b46afa844f8ab7874dd530daa1d23fd85707511d
systems-hassan <hassan.nasir@systemsltd.com>
2023-06-23 16:03:15 +0500

    Assignment 1 Read File

---
+++ README.md
@@ -0,0 +1,26 @@
+
+## Submitted By
+
+[Hassan Nasir](https://www.github.com/systems-hassan)
+
+## How to run
+
+#### npm install
+
+```http
+  npm install --verbose
+```
+
+#### npm start
+
+```http
+  npm start
+```
+
+## Assignment 1
+
+- Read File
+- Display the response on browser
+
+[See Result](http://localhost:3000/files)
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.setHeader('Content-Type', 'application/json');
+  const fileStream = fs.createReadStream(path.join(__dirname, '../data.json'), { encoding: 'utf8' });
+
+  fileStream.pipe(res);
 });
 
 module.exports = router;
